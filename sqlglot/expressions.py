6c2e5107e9aa86abc31206a4f18d58f6d8bc09ae
vaggelisd <daniasevangelos@gmail.com>
2024-08-19 18:03:17 +0300

    PR Feedback 1

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -5325,7 +5325,7 @@ class Unnest(Func, UDTF):
         "expressions": True,
         "alias": False,
         "offset": False,
-        "nested_array": False,
+        "explode_array": False,
     }
 
     @property
